a4491dfe84b6411af0a14fd14cebfe734159d07f
Tallis <ruicosta@ieee.org>
2014-09-04 13:14:48 +0100

    First commit of a shamefull code

---
+++ Vision.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0028)http://www.kairossociety.hu/ -->
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US"><head profile="http://gmpg.org/xfn/11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+
+
+<?php include("header.php"); ?>
+
+<!--THIS IS THE SLIDER ZONE!-->
+
+<div class="slider_wrap" style="">
+
+<div id="zn_slider">
+
+	<div id="slider" class="easyslider" style="width: 1000px; height: 310px; overflow: hidden;">
+			<img width="900px"  style="margin-top:-65px" src="./images/WorldMap.png" class="attachment-post-thumbnail wp-post-image" alt="Kairos Society Portugal" title="Kairos Society Portugal">
+		
+</div>
+
+	 </div>
+
+
+       <!--  <div id="zn_slider">
+
+        
+
+<div id="slider" class="easyslider" style="width: 1000px; height: 310px; overflow: hidden;">
+
+        <ul style="width: 3000px; margin-left: 0px;">
+        <li>	
+        <a href=""><img width="1150" height="350" src="./KairosFiles/4.jpg" class="attachment-post-thumbnail wp-post-image" alt="4" title="4"><div style="background:url(&#39;http://www.kairossociety.hu/images/transparent.png&#39;); width:1000px; height:306px; position:absolute; top:0;z-index:1001;"></div></a>
+
+            </li>
+
+        
+       </ul>
+
+</div>
+        </div> -->
+
+    
+
+
+
+</div>
+<div style="height:1px;border-bottom: 1px dashed #666666;clear:both;margin-bottom:15px;position:relative;top:400px"></div>
+
+
+
+
+    <div class="center">
+
+<!--HEADER START-->
+
+
+<!--HEADER END-->
+
+
+<!--SLIDER END-->
+
+
+
+
+
+
+<!--SLIDER END-->
+
+
+
+<!--MIDROW STARTS-->
+
+
+
+<!--MIDROW END-->
+
+  <div class="single_post_full">
+               <div class="post_content">
+                <h2 class="postitle">KAIROS SOCIETY PORTUGAL WEBSITE NOTIFICATION</h2>
+                <center>
+                <img src="./KairosFiles/line3.png" class="line3"></center>
+
+<div class="textbody">
+   <h3 >We are currently building awesome stuff, stay tuned</h3>
+</br>
+
+
+	</br></br>
+	</br></br>
+	</br></br>
+</div>
+                    </div> 
+                    
+                   
+                      </div>
+                               
+               
+
+
+<!--LATEST POSTS-->
+
+
+	<div class="lay1">
+
+<div class="lay1_wrap">
+
+
+                   
+</div>
+
+            
+            <div class="hidden_nav"></div>
+
+
+
+    </div>
+
+<!--LATEST POSTS END-->
+
+
+</div>
+	<!--COPYRIGHT TEXT-->
+
+    <div id="copyright">
+    	<div id="wrapper">
+
+            <div id="left">
+            	<img src="./KairosFiles/footer_k.png">
+                <div style="clear:both"></div>                 
+                <p>© 2013 Kairos Society Portugal<br>
+                
+All Rights Reserved.</p>
+
+            </div>
+            <div id="right">
+          White sponsors logo
+           
+            </div>
+	
+        <!--FOOTER MENU-->    
+		</div>
+           
+
+    </div>
+
+
+    <script type="text/javascript">
+		/* <![CDATA[ */
+    jQuery(function(){
+	jQuery("#slider").easySlider({
+		auto: true,
+		continuous: true,
+		numeric: true,
+		pause: 10000		});
+	});
+	/* ]]> */
+	</script>
+    
+ 
+<script type="text/javascript">
+/* <![CDATA[ */ 
+//AJAX PAGINATION
+jQuery(document).ready(function(){
+jQuery('.amp_next, .amp_prev').css({"display":"none"});
+jQuery('.znn_paginate a:first').each(function () {
+    var href = jQuery(this).attr('href');
+    jQuery(this).attr('href', href + '?paged=1');
+});
+
+jQuery('.znn_paginate a').each(function(){
+        this.href = this.href.replace('/page/', '?paged=');
+});
+    jQuery('.znn_paginate a').live('click', function(e)  {
+	jQuery('.znn_paginate a, span.amp_page').removeClass('amp_current'); // remove if already existant
+    jQuery(this).addClass('amp_current');
+
+	e.preventDefault();
+			var link = jQuery(this).attr('href');
+	jQuery('.lay1_wrap').html('<div class="zn_ajaxwrap"><div class="zn_ajax"></div></div>').load(link + '.lay1_wrap .post', function() {
+			var divs = jQuery(".lay1 .post");
+			for(var i = 0; i < divs.length; i+=3) {
+			  divs.slice(i, i+3).wrapAll("<div class='zn_row'></div>"); }
+    jQuery('.lay1_wrap').fadeIn(500); });
+	
+	
+    });
+});  // end ready function
+/* ]]> */
+</script> 
+
+
+    <script type="text/javascript" src="./KairosFiles/jquery.form.min.js"></script>
+<script type="text/javascript">
+/* <![CDATA[ */
+var _wpcf7 = {"loaderUrl":"http:\/\/www.kairossociety.hu\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+/* ]]> */
+</script>
+<script type="text/javascript" src="./KairosFiles/scripts.js"></script>
+
+
+
+<div id="fancybox-tmp"></div><div id="fancybox-loading"><div></div></div><div id="fancybox-overlay"></div><div id="fancybox-wrap"><div id="fancybox-outer"><div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div><div id="fancybox-content"></div><a id="fancybox-close"></a><div id="fancybox-title"></div><a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a><a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a></div></div></body></html>
---
+++ WhoWeAre.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0028)http://www.kairossociety.hu/ -->
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US"><head profile="http://gmpg.org/xfn/11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+
+<?php include("header.php"); ?>
+
+<!--THIS IS THE SLIDER ZONE!-->
+
+<div class="slider_wrap" style="">
+
+<div id="zn_slider">
+
+	<div id="slider" class="easyslider" style="width: 1000px; height: 310px; overflow: hidden;">
+			<img width="900px"  style="margin-top:-65px" src="./images/WorldMap.png" class="attachment-post-thumbnail wp-post-image" alt="Kairos Society Portugal" title="Kairos Society Portugal">
+		
+</div>
+
+	 </div>
+
+
+       <!--  <div id="zn_slider">
+
+        
+
+<div id="slider" class="easyslider" style="width: 1000px; height: 310px; overflow: hidden;">
+
+        <ul style="width: 3000px; margin-left: 0px;">
+        <li>	
+        <a href=""><img width="1150" height="350" src="./KairosFiles/4.jpg" class="attachment-post-thumbnail wp-post-image" alt="4" title="4"><div style="background:url(&#39;http://www.kairossociety.hu/images/transparent.png&#39;); width:1000px; height:306px; position:absolute; top:0;z-index:1001;"></div></a>
+
+            </li>
+
+        
+       </ul>
+
+</div>
+        </div> -->
+
+    
+
+
+
+</div>
+<div style="height:1px;border-bottom: 1px dashed #666666;clear:both;margin-bottom:15px;position:relative;top:400px"></div>
+
+
+
+
+    <div class="center">
+
+<!--HEADER START-->
+
+
+<!--HEADER END-->
+
+
+<!--SLIDER END-->
+
+
+
+
+
+
+<!--SLIDER END-->
+
+
+
+<!--MIDROW STARTS-->
+
+
+
+<!--MIDROW END-->
+
+  <div class="single_post_full">
+               <div class="post_content">
+                <h2 class="postitle">KAIROS SOCIETY PORTUGAL</h2>
+                <center>
+                <img src="./KairosFiles/line3.png" class="line3"></center>
+
+<div class="textbody">
+  <p dir="ltr">Kairos Society Portugal started in late 2012 and has been actively linking  the most entrepreneurial and outstanding students in Portugal.  </p>
+  <p dir="ltr">Kairos Society Portugal aims to connect the young entrepreneurs and visionary leaders to fellows in other countries and reputable mentors that will help our fellows taking their projects to the next level. We are building the basis for those who dare to think big to bring their visions into reality.  </p>
+  <p dir="ltr">Currently we have members that are part of startups that aim to disrupt the industry they are in, founders of entrepreneurship clubs, CEOs of microfinance organizations  and other future leaders.  </p>
+</br>
+<h2 class="postitle">KAIROS PORTUGAL FELLOWS</h2>
+  <br />
+  <p dir="ltr"><b>Ana Margarida Marcos</b> </p>
+  <p dir="ltr"><a href="mailto: margaridamarcos@hotmail.com">margaridamarcos@hotmail.com</a> </p>
+  <p dir="ltr">Margarida is the Co-CEO of C-MOVE, a microfinance student run organization that operates in Portugal, Mozambique, São Tomé e Príncipe and Timor-Leste, since 2012. Before, Margarida worked for 6 months as a fellow of C-MOVE in Mozambique helping with microfinance and consultancy to local businesses, coaching entrepreneurs and developing new ways of developing the local businesses and the educational system. With a degree in Management at Nova School of Business and Economics (2008-2011), she is currently concluding a Marketing internship at Henkel, to go back to Nova SBE for a Management Masters. </p>
+  <br />
+  <p dir="ltr"><b>André Albuquerque</b> </p>
+  <p dir="ltr"><a href="mailto: albuquerque.a.andre@gmail.com">albuquerque.a.andre@gmail.com</a> </p>
+  <p dir="ltr">André Albuquerque is a Master business student majoring in Technology Entrepreneurship and Innovation Strategy at Católica Lisbon School of Business and Economics. </p>
+  <br />
+  <p dir="ltr">He worked at Google as Strategic Partnerships Intern both at European HQ in Dublin and Global HQ in California. Previously he won the 2012 Google Europe Ambassador program and was selected to the Associate Summer Internship program at Google in 2011. </p>
+  <p dir="ltr">André has worked multinational companies, small businesses, start-ups, NGO's, entrepreneurship organizations and education entities. </p>
+  <br />
+  <p dir="ltr"><b>Catarina Martins</b> </p>
+  <p dir="ltr"><a href="mailto:Catarinamartins@ieee-ist.org">Catarinamartins@ieee-ist.org</a> </p>
+  <p dir="ltr">Catarina is a student of  Industrial Management Engineering, at IST. She is an active member of a student branch of IEEE, taking part in several social and academic projects. Lately she coordinated an entrepreneurial event and competition in Mobile Apps. She's also a member of: NEEGI, promoting the binding of technological knowledge with management skills, and fostering interchange of students in Europe; ESTIEM, as delegate from Lisbon, and member of the Organizing Committee of the next European Council Meeting. She also likes to collaborate with companies in events like entrepreneurship boot camps and team-building activities. </p>
+  <br />
+  <p dir="ltr"><b>David Dias</b> </p>
+  <p dir="ltr"><a href="mailto: daviddias.p@gmail.com">daviddias.p@gmail.com</a> </p>
+  <p dir="ltr">David is a Communication Networks Engineer, taking now a Masters on Cloud Computing, P2P networks and Mobile applications at IST,. He is, as well, an IEEE Volunteer serving as IEEE Computer Society Student Ambassador for Region 8(EMEA) and the Google Student Ambassador for Europe representing Portugal. David lives his days trying to push forward the humanity by being a catalyst of his peers, trying to make the most of the energy he puts in every project he is up too, whether it is related to his field of studies or not. </p>
+  <p dir="ltr">You can find David motivating everyone around me to make something happen, or trying to develop the next big thing in WebDev. </p>
+  <br />
+  <p dir="ltr"><b>Davis Gouveia</b> </p>
+  <p dir="ltr"><a href="mailto:davis.gouveia@gmail.com">davis.gouveia@gmail.com</a> </p>
+  <p dir="ltr">Passionate about entrepreneurship, education and new technologies, with only 18 years old helped to found one that ended up becoming one of the most popular educational portals in Portugal, <a href="http://www.exames.org/">www.exames.org</a>. During the next 5 years he continued with this project and, in the meanwhile,  became tge  president at JUNITEC, one of the most innovative junior enterprises in the country. Since then he is vice president at JADE Portugal, the Portuguese Federation of Junior Enterprises, part of the organizingteam member of Startup Pirates @ Lisboa and founding member of the Daring Project. </p>
+  <br />
+  <p dir="ltr"><b>João Figueirinhas Costa</b> </p>
+  <p dir="ltr"><a href="mailto:joao.figcosta@gmail.com">joao.figcosta@gmail.com</a> </p>
+  <p dir="ltr">João is a naturally born networker and communicator with an entrepreneurial spirit, wanting to turn our world into a more conscious, sustainable and healthy ecosystem, in all ways. He believes innovations in education and public governance are two of the main ways in which his objective, shared by many, can be attained. João is a senior student at the Portuguese Catholic University, where he is studying economics. Over the last 3 years he has worked in Portugal and China, in a variety of sectors, including Talent Management and Recruitment, Hotel Management, B2C Tourism Industry, Banking and Tech Startups such as Groupon and Wimdu.  </p>
+  <br />
+  <p dir="ltr"><b>João Freire de Andrade</b> </p>
+  <p dir="ltr"><a href="mailto: j.freire.andrade@gmail.com">j.freire.andrade@gmail.com</a> </p>
+  <p dir="ltr">João is the founder of BET, Bring Entrepreneurs Together. BET&rsquo;s mission is to promote entrepreneurship by creating opportunities to gather students from different Universities and areas of knowledge. João is the founder and CEO of t-shirt lovers, a web based startup that have as mission the objective of creating the biggest clothing community. He has also been a MOVE Micro Finance fellow in Mozambique. In this project he helped 7 poor entrepreneurs starting their businesses.  </p>
+  <p dir="ltr">His under degree in economics was in NOVA and is currently finishing the thesis in Católica Lisbon, with a focus on startup valuation.  </p>
+  <br />
+  <p dir="ltr"><b>José Pedro Silva</b>  </p>
+  <p dir="ltr"><a href="mailto: jpedrogmsilva@gmail.com">jpedrogmsilva@gmail.com</a> </p>
+  <p dir="ltr">19-year-old José Pedro studies, simultaneously, Mathematics at the University of Porto and Management at the Catholic University of Portugal (UCP) with a merit scholarship. He currently is the CFO of Católica Students&rsquo; Corporation, UCP&rsquo;s junior enterprise, and an active member in other projects such as the School of Economics and Management&rsquo;s Debate Club and its academic newspaper, while also preparing students to take the BMAT. </p>
+  <p dir="ltr">Interested and passionate about everything he does, José Pedro is searching for the best way to leave its mark in this world. He is known as a results-driven and persistent person with remarkable analytical and execution skills. Capable of solving problems efficiently, has a great will to keep doing even more and better. </p>
+  <br />
+  <p dir="ltr"><b>Mario Nzualo</b>  </p>
+  <p dir="ltr"><a href="mailto: mario.nzualo@gmail.com">mario.nzualo@gmail.com</a> </p>
+  <p dir="ltr">Originally from Mozambique, has spent half of his life in Portugal and half in Mozambique.Currently living in Lisbon and finishing his Master Degree in Communication Networks Engineering and specializing in Mobile Payments.  </p>
+  <p dir="ltr">Loves to travel and meet new people and cultures. Spends most of his time traveling all over Europe while leading a pan-European organisation of 4200 technical students, BEST (Board of European Students of Technology). </p>
+  <br />
+  <p dir="ltr"><b>Pedro Balas</b> </p>
+  <p dir="ltr"><a href="mailto: pedrobalas5@gmail.com">pedrobalas5@gmail.com</a> </p>
+  <p dir="ltr">Pedro is a student of Mechanical Engineering at IST- In 2008, he invented the internationally patented Electric Spherical Generator (ESG). In 2009, he founded the company EMOVE, becoming CEO, in order to accomplish the goal of taking the ESG to several markets, with a stronger focal point in the wave energy industry. Between 2009 and 2010, along with his company, he won seven distinct prizes. Since June 2011, he established a new office in Silicon Valley, California, through an invitation of the incubator Plug and Play Technology Center. By the Summer of 2012, EMOVE patented its technology internationally, and built and tested the most efficient electromechanical converter in the world.  His major objectives in three years are to make wave energy economically viable and capacitate wind turbines with this efficient technology, and to build and test real scale prototypes for both applications.  </p>
+  <br />
+  <p dir="ltr"><b>Pedro Conceição</b> </p>
+  <p dir="ltr"><a href="mailto:Pedro.conceicao@kairossociety.org">Pedro.conceicao@kairossociety.org</a> </p>
+  <p dir="ltr">Pedro is the founding presidente of Kairos Portugal. While doing his masters at the Maastricht University, he decided it would make perfect sense to connect the most outstanding Portuguese students to their peers around the world. Pedro has studied and worked in a number of countries such as Brazil, China and India, among others. In the meanwhile he has founded, with friends, a youth nonprofit organization and a startup. Currently he is working with social entrepreneurs in Lisbon.  </p>
+  <br />
+  <p dir="ltr"><b>Raquel Inácio </b>  </p>
+  <p dir="ltr"><a href="mailto: raquel.inacio24@gmail.com">raquel.inacio24@gmail.com</a> </p>
+  <p dir="ltr">Master student of Electrical and Computer Engineer at IST. Since the first year of university she has participated in new projects and technological activities. She joined JUNITEC (first junior company in Portugal) in 2009, and since then has evolved and nowadays is the President. Concerned not only with the development of technology and entrepreneurship, but also with the society and human relations, she did volunteer work in a local Hospital with children. Fond of sports, practices tennis, mountain biking and jogging. </p>
+  <p dir="ltr"><b>Rui Costa</b> </p>
+  <p dir="ltr"><a href="mailto: ruicosta@ieee.org">ruicosta@ieee.org</a> </p>
+  <p dir="ltr">Rui Costa is a Network and Communications Engineering Masters student with deep concerns in making a change in several technological and academic areas, having as main research and thesis topic Vehicular Ad-hoc Networks. Currently he is working on his master thesis that aims to develop an agnostic application development platform for vehicular networks environments that facilitates applications development while creating a low cost alternative to such systems. </p>
+  <p dir="ltr">Rui is also an experienced presenter and has led several presentations to different targets of several topics. Currently he is leading a global project for IEEE that aims to develop and spread the creation of local education resources based on multimedia resources and technologies in several countries. </p>
+  <br />
+  <p dir="ltr"><b>Vasco Sousa</b>  </p>
+  <p dir="ltr"><a href="mailto:vasco2sousa@gmail.com">vasco2sousa@gmail.com</a> </p>
+  <p dir="ltr">Born in São Mamede de Infesta, county of Matosinhos, in March 1989, Vasco Sousa is a student of the Integrated Master of Industrial Engineering and management of the Engineering Faculty of the University of Porto. He is currently the consultant of Research and Development at Hidros, the junior enterprise of engineering of the State University of Rio de Janeiro, the institution where is doing a student exchange program. In October 2009 Vasco Sousa joined the management board of the Entrepreneurship Club of the University of Porto (CEdUP), responsible for the dissemination of the entrepreneurship spirit among the academic community, assuming the position of president of the organization in January 2011. </p>
+  <br />
+  <p>&nbsp;</p>
+
+	</br></br>
+	</br></br>
+	</br></br>
+</div>
+                    </div> 
+                    
+                   
+                      </div>
+                               
+               
+
+
+<!--LATEST POSTS-->
+
+
+	<div class="lay1">
+
+<div class="lay1_wrap">
+
+
+                   
+</div>
+
+            
+            <div class="hidden_nav"></div>
+
+
+
+    </div>
+
+<!--LATEST POSTS END-->
+
+
+</div>
+	<!--COPYRIGHT TEXT-->
+
+    <div id="copyright">
+    	<div id="wrapper">
+
+            <div id="left">
+            	<img src="./KairosFiles/footer_k.png">
+                <div style="clear:both"></div>                 
+                <p>© 2013 Kairos Society Portugal<br>
+                
+All Rights Reserved.</p>
+
+            </div>
+            <div id="right">
+          White sponsors logo
+           
+            </div>
+	
+        <!--FOOTER MENU-->    
+		</div>
+           
+
+    </div>
+
+
+    <script type="text/javascript">
+		/* <![CDATA[ */
+    jQuery(function(){
+	jQuery("#slider").easySlider({
+		auto: true,
+		continuous: true,
+		numeric: true,
+		pause: 10000		});
+	});
+	/* ]]> */
+	</script>
+    
+ 
+<script type="text/javascript">
+/* <![CDATA[ */ 
+//AJAX PAGINATION
+jQuery(document).ready(function(){
+jQuery('.amp_next, .amp_prev').css({"display":"none"});
+jQuery('.znn_paginate a:first').each(function () {
+    var href = jQuery(this).attr('href');
+    jQuery(this).attr('href', href + '?paged=1');
+});
+
+jQuery('.znn_paginate a').each(function(){
+        this.href = this.href.replace('/page/', '?paged=');
+});
+    jQuery('.znn_paginate a').live('click', function(e)  {
+	jQuery('.znn_paginate a, span.amp_page').removeClass('amp_current'); // remove if already existant
+    jQuery(this).addClass('amp_current');
+
+	e.preventDefault();
+			var link = jQuery(this).attr('href');
+	jQuery('.lay1_wrap').html('<div class="zn_ajaxwrap"><div class="zn_ajax"></div></div>').load(link + '.lay1_wrap .post', function() {
+			var divs = jQuery(".lay1 .post");
+			for(var i = 0; i < divs.length; i+=3) {
+			  divs.slice(i, i+3).wrapAll("<div class='zn_row'></div>"); }
+    jQuery('.lay1_wrap').fadeIn(500); });
+	
+	
+    });
+});  // end ready function
+/* ]]> */
+</script> 
+
+
+    <script type="text/javascript" src="./KairosFiles/jquery.form.min.js"></script>
+<script type="text/javascript">
+/* <![CDATA[ */
+var _wpcf7 = {"loaderUrl":"http:\/\/www.kairossociety.hu\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","sending":"Sending ..."};
+/* ]]> */
+</script>
+<script type="text/javascript" src="./KairosFiles/scripts.js"></script>
+
+
+
+<div id="fancybox-tmp"></div><div id="fancybox-loading"><div></div></div><div id="fancybox-overlay"></div><div id="fancybox-wrap"><div id="fancybox-outer"><div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div><div id="fancybox-content"></div><a id="fancybox-close"></a><div id="fancybox-title"></div><a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a><a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a></div></div></body></html>
